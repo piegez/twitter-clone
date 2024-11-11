bf17b60d95c2b546126a06a51c5d808892b94c7e
piegez <jessicamsampaio@gmail.com>
2024-11-11 11:11:24 -0300

    add: python anywhere

--- core/tests.py
+++ core/tests.py
@@ -16,12 +16,12 @@ class TweetFactory(DjangoModelFactory):
     class Meta:
         model = Tweet
 
-    user = factory.SubFactory(UserFactory)  # SubFactory cria um usuário antes
+    user = factory.SubFactory(UserFactory)
     content = 'Teste de tweet'
 
 
 class TweetTestCase(TestCase):
     def test_tweet_creation(self):
-        tweet = TweetFactory()  # Isso cria o usuário e o tweet corretamente
+        tweet = TweetFactory()
         self.assertEqual(tweet.content, 'Teste de tweet')
-        self.assertIsNotNone(tweet.user)  # Verifica se o tweet tem um usuário associado
+        self.assertIsNotNone(tweet.user)
